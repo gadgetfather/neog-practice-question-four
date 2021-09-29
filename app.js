981c51e49e60563af9cf1560ac34c3a00ff2ba8c
Akshay Kamble <kamblea635@gmail.com>
2021-09-29 09:54:43 +0530

    completed app

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ var redBtn = document.querySelector("#red-btn")
 var greenBtn = document.querySelector("#green-btn")
 var blueBtn = document.querySelector("#blue-btn")
 var outputEl = document.querySelector("#output")
+
+
+redBtn.addEventListener('click', function goRed(){
+    var inputData = input.value
+    outputEl.textContent = inputData
+    outputEl.style.color = "red"
+
+})
+
+greenBtn.addEventListener('click', function goGreen(){
+    var inputData = input.value
+    outputEl.textContent = inputData
+    outputEl.style.color = "green"
+})
+
+blueBtn.addEventListener('click', function goBlue(){
+    var inputData = input.value
+    outputEl.textContent = inputData
+    outputEl.style.color = "blue"
+})
